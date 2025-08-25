798c04cb3d20978dfa511aa68201efe5e572ebbe
lokesph8 <lokesph8@gmail.com>
2025-08-25 11:04:16 +0530

    Updated project with latest changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Cart from './pages/Cart/Cart'
 import Home from './pages/Home/Home'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
@@ -17,7 +17,8 @@ const App = () => {
     <div className='app'>
       <Navbar setShowLogin={setShowLogin}/>
       <Routes>
-        <Route path='/' element={<Home/>} />
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path='/home' element={<Home/>} />
         <Route path='/cart' element={<Cart/>} />
         <Route path='/order' element={<PlaceOrder/>} />
       </Routes>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import StoreContextProvider from './context/StoreContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename='/The-SpicyTomato-Food-Restaurant'>
     <StoreContextProvider>
       <App />
     </StoreContextProvider> 
